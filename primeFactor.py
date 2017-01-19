1384724de0a4940f25216607805ef86ecabb1e33
William Muhlbach <applesarefum>
2017-01-18 20:45:30 -0500

    save

--- primeFactor.py
+++ primeFactor.py
@@ -1,36 +1,37 @@
 #author=Nathan Jenkins
 
-def is_this_prime(number):
-  j = True
-  if number == 1:
-    return False
-  elif number == 2:
-    return True
-  else:
-    for i in range(2, number):
-      if number % i == 0:
+def is_prime(number):
+    j = None
+    if number == 1:
+        return False
+    elif number == 2:
+        return True
+    else:
+        for i in range(2, number):
+    if number % i == 0:
         j = False
-  return j
-if __name__ = '__main__':
-  for i in range (1, 11):
-   print is_this_prime(i)
+    return j
 #-------------------------------------
-def factoize(number):
-  list_of_factors = [1]
-  if number == 1:
-	list_of_factors = [1]
-  else:
-	for i in range(2, number+1):
-  	if number % i == 0:
-    	list_of_factors.append(i)
-  return list_of_factors
-#for i in range(1, 11):
-  #print factors(i)
+def factorize(number):
+    list_of_factors = [1]
+    if number == 1:
+        list_of_factors = [1]
+    else:
+        for i in range(2, number+1):
+    if number % i == 0:
+        list_of_factors.append(i)
+    return list_of_factors
+#-------------------------------------
 def GCF(x,y):
-  common_factors = []
-  for i in factors(x):
-	if y % i == 0:
-  	common_factors.append(i)
-  return common_factors[len(common_factors)-1]
-print GCF(55,88)
-
+    common_factors = []
+    for i in factors(x):
+        if y % i == 0:
+            common_factors.append(i)
+    return common_factors[len(common_factors)-1]
+#-------------------------------------
+if __name__ = '__main__':
+    print GCF(55,88)
+    for i in range (1, 11):
+        print is_this_prime(i)
+    for i in range(1, 11):
+        print factors(i)
